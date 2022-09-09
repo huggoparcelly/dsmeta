d228e5f8b07fd51b935f9de739025ecb6a97da67
Huggo Parcelly <h.parcelly@gmail.com>
2022-09-08 21:08:15 -0300

    add default date

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/serviceImp/SaleServiceImp.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/serviceImp/SaleServiceImp.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleServiceImp implements SaleService {
@@ -19,8 +21,10 @@ public class SaleServiceImp implements SaleService {
     @Override
     public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 
-        LocalDate min = LocalDate.parse(minDate);
-        LocalDate max = LocalDate.parse(maxDate);
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusDays(360) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 
         return saleRepository.findSales(min, max, pageable);
     }
