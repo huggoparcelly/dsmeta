1cf0bb333fb5011fd69a6f32b6939fe1c0c27b4e
Huggo Parcelly <h.parcelly@gmail.com>
2022-09-11 20:27:46 -0300

    send notification

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,7 +3,6 @@ package com.devsuperior.dsmeta.controllers;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
 import com.devsuperior.dsmeta.services.SmsService;
-import com.devsuperior.dsmeta.services.serviceImp.SmsServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
